d4f7d08bb906c89c5f9d2e8fe33930bc9e4ceba5
NathanNux <139380259+NathanNux@users.noreply.github.com>
2025-08-14 02:50:20 +0200

    updating frontend to function with the old setup of the home, store and products storefront.

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -1,23 +1,38 @@
 import { Metadata } from "next"
 
-import FeaturedProducts from "@modules/home/components/featured-products"
-import Hero from "@modules/home/components/hero"
-import { getCollectionsWithProducts } from "@lib/data/collections"
+// import Hero from "@modules/home/components/original/hero"
+// import FeaturedProducts from "@modules/home/components/original/featured-products"
+import { listCollections } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
+import Hero from "@modules/home/components/hero"
+import FeaturedProducts from "@modules/home/components/featured-products"
+// import IntroHero from "@modules/home/components/Hero/IntroHero"
+// import AboutHero from "@modules/home/components/Hero/AboutHero"
+// import ECom from "@modules/home/components/E-com"
+// import Kurzy from "@modules/home/components/Kurzy"
+// import Info from "@modules/home/components/Info"
+// import ScrollToTopOnReload from "@lib/helpers/scrollToTopOnReload"
+// import HeroSection from "@modules/home/components/Hero"
 
 export const metadata: Metadata = {
   title: "Medusa Next.js Starter Template",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "A performant frontend ecommerce starter template with Next.js 15 and Medusa.",
 }
 
-export default async function Home({
-  params: { countryCode },
-}: {
-  params: { countryCode: string }
+export default async function Home(props: {
+  params: Promise<{ countryCode: string }>
 }) {
-  const collections = await getCollectionsWithProducts(countryCode)
+  // this is the main page of the storefront
+  const params = await props.params
+
+  const { countryCode } = params
+
   const region = await getRegion(countryCode)
+
+  const { collections } = await listCollections({
+    fields: "id, handle, title",
+  })
 
   if (!collections || !region) {
     return null
@@ -25,6 +40,11 @@ export default async function Home({
 
   return (
     <>
+      {/* <ScrollToTopOnReload />
+      <HeroSection />
+      <ECom />
+      <Kurzy />
+      <Info /> */}
       <Hero />
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
--- storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -1,48 +1,65 @@
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
-
-import ProductTemplate from "@modules/products/templates"
+import { getBundleProduct, getProductReviews, listProducts } from "@lib/data/products"
 import { getRegion, listRegions } from "@lib/data/regions"
-import { getProductByHandle, getProductsList } from "@lib/data/products"
+import Product from "@modules/products/ProductPage/product"
+import Details from "@modules/products/ProductPage/details"
+import SoldProducts from "@modules/products/ProductPage/Sold"
+import { listCategories } from "@lib/data/categories"
+import ProductReviews from "@modules/products/components/product-reviews"
+import ProductTemplate from "@modules/products/templates"
+import BundleActions from "@modules/products/components/bundle-actions"
 
 type Props = {
-  params: { countryCode: string; handle: string }
+  params: Promise<{ countryCode: string; handle: string }>
 }
+
+const ENABLE_BUNDLES = true
 
 export async function generateStaticParams() {
-  const countryCodes = await listRegions().then(
-    (regions) =>
-      regions
-        ?.map((r) => r.countries?.map((c) => c.iso_2))
-        .flat()
-        .filter(Boolean) as string[]
-  )
-
-  if (!countryCodes) {
-    return null
-  }
-
-  const products = await Promise.all(
-    countryCodes.map((countryCode) => {
-      return getProductsList({ countryCode })
-    })
-  ).then((responses) =>
-    responses.map(({ response }) => response.products).flat()
-  )
-
-  const staticParams = countryCodes
-    ?.map((countryCode) =>
-      products.map((product) => ({
-        countryCode,
-        handle: product.handle,
-      }))
+  try {
+    const countryCodes = await listRegions().then((regions) =>
+      regions?.map((r) => r.countries?.map((c) => c.iso_2)).flat()
     )
-    .flat()
 
-  return staticParams
+    if (!countryCodes) {
+      return []
+    }
+
+    const promises = countryCodes.map(async (country) => {
+      const { response } = await listProducts({
+        countryCode: country,
+        queryParams: { limit: 100, fields: "handle" },
+      })
+
+      return {
+        country,
+        products: response.products,
+      }
+    })
+
+    const countryProducts = await Promise.all(promises)
+
+    return countryProducts
+      .flatMap((countryData) =>
+        countryData.products.map((product) => ({
+          countryCode: countryData.country,
+          handle: product.handle,
+        }))
+      )
+      .filter((param) => param.handle)
+  } catch (error) {
+    console.error(
+      `Failed to generate static paths for product pages: ${
+        error instanceof Error ? error.message : "Unknown error"
+      }.`
+    )
+    return []
+  }
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const params = await props.params
   const { handle } = params
   const region = await getRegion(params.countryCode)
 
@@ -50,40 +67,97 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     notFound()
   }
 
-  const product = await getProductByHandle(handle, region.id)
+  const product = await listProducts({
+    countryCode: params.countryCode,
+    queryParams: { handle },
+  }).then(({ response }) => response.products[0])
 
   if (!product) {
     notFound()
   }
 
   return {
-    title: `${product.title} | Medusa Store`,
+    title: `${product.title} | Keramická Zahrada`,
     description: `${product.title}`,
     openGraph: {
-      title: `${product.title} | Medusa Store`,
+      title: `${product.title} | Keramická Zahrada`,
       description: `${product.title}`,
       images: product.thumbnail ? [product.thumbnail] : [],
     },
   }
 }
 
-export default async function ProductPage({ params }: Props) {
+export default async function ProductPage(props: Props) {
+  const params = await props.params
   const region = await getRegion(params.countryCode)
 
   if (!region) {
     notFound()
   }
 
-  const pricedProduct = await getProductByHandle(params.handle, region.id)
+   // @ts-ignore 
+  const pricedProduct = await listProducts({
+    countryCode: params.countryCode,
+    queryParams: { 
+      handle: params.handle, 
+      fields: "*bundle, *variants.calculated_price, +variants.inventory_quantity, +metadata, +tags",
+    },
+  }).then(({ response }) => response.products[0])
+
+  // Check before using
   if (!pricedProduct) {
     notFound()
   }
+
+  // Fetch all categories (with products)
+  const categories = await listCategories({
+    fields: "*category_children, *products, *parent_category, *parent_category.parent_category",
+    limit: 10,
+  })
+
+  // Find categories this product belongs to
+  const productCategories = categories.filter(cat =>
+    Array.isArray(cat.products) && cat.products.some(p => p.id === pricedProduct.id)
+  )
+
+  const bundleProduct = pricedProduct.bundle ? 
+    await getBundleProduct(pricedProduct.bundle.id, {
+      currency_code: region?.currency_code,
+      region_id: region?.id,
+    }) : null
+
+
+  const reviewsData = await getProductReviews({
+    productId: pricedProduct.id,
+    limit: 10,
+    offset: 0,
+  })
+
+  console.log("reviewsData", reviewsData)
+  console.log("pricedProductID", pricedProduct.id)
 
   return (
     <ProductTemplate
       product={pricedProduct}
       region={region}
       countryCode={params.countryCode}
+      bundle={bundleProduct?.bundle_product}
     />
+    // <main>
+    //  <Product
+    //     product={pricedProduct}
+    //     region={region}
+    //     countryCode={params.countryCode}
+    //     categories={productCategories}
+    //   />
+    //   {ENABLE_BUNDLES && bundleProduct && (
+    //     <BundleActions 
+    //       bundle={bundleProduct?.bundle_product}
+    //     />
+    //   )}
+    //   <Details product={pricedProduct} />
+    //   <ProductReviews productId={pricedProduct.id} initialReviews={reviewsData.reviews} initialRating={reviewsData.average_rating} initialCount={reviewsData.count} />
+    //   <SoldProducts />
+    // </main>
   )
 }
