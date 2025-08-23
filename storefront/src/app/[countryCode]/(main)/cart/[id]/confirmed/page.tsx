824f2109fae4b27cc6462acd4a5df0b6f13bb355
vencoslav1357 <155269304+vencoslav1357@users.noreply.github.com>
2025-08-23 20:08:41 +0200

    Update page.tsx

--- storefront/src/app/[countryCode]/(main)/cart/[id]/confirmed/page.tsx
+++ storefront/src/app/[countryCode]/(main)/cart/[id]/confirmed/page.tsx
@@ -1,7 +1,6 @@
-import { retrieveOrder } from "@lib/data/orders"
-import OrderCompletedTemplate from "@modules/order/templates/order-completed-template"
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
+import { placeOrder } from "@lib/data/cart"
 
 type Props = {
   params: Promise<{ id: string }>
@@ -13,12 +12,14 @@ export const metadata: Metadata = {
 
 export default async function OrderConfirmedPage(props: Props) {
   const params = await props.params
-  const order = await retrieveOrder(params.id).catch(() => null)
+  if (!params.id) {
+    return notFound()
+  }
+  const order = await placeOrder(params.id).catch(() => null)
   console.log("order:", order)
 
   if (!order) {
     return notFound()
   }
 
-  return <OrderCompletedTemplate order={order} />
 }
